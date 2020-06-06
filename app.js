8a5396eeb4695fcfaca17109c5320a4de5b6fdf2
Muhammad Bilal <bilalmohib7896@gmail.com>
2020-06-06 13:30:05 +0500

    This is the assignment 4 School home page

---
+++ app.js
@@ -0,0 +1,37 @@
+alert("Thanks to saylani and sir muhammad ali mughal");
+// function myFunction() {
+//     var x = document.getElementById("logo2");
+//     if (x.style.display = 'block') {
+//         div.style.display = 'none';
+//     }
+//     else {
+//         x.style.display = 'none';
+//     }
+    
+//   }
+// document.getElementById("_bar_Id_").onclick = function() {
+//     var x = document.getElementById("logo2");
+//     if (x.style.visibility === "hidden") {
+//     x.style.visibility = "visible";
+//     } else {
+//       x.style.visibility = "hidden";
+//       }
+//     }
+// llllllllllllllllllllllllllll
+// var button = document.getElementById('button');
+// var button = document.getElementById('button1');
+// var button = document.getElementById('button2');
+
+// button.onclick = function() {
+//     var div = document.getElementById('newpost');
+//     var div1 = document.getElementById('newpost1');
+//     var div2 = document.getElementById('newpost2');
+//     if (div.style.display !== 'none') {
+//         div.style.display = 'none';
+//     }
+//     else{
+//         div.style.display = 'block';
+//     }
+
+// };
+  
